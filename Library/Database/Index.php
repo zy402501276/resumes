c01105b4e7d709ae21dd25ea3da977ec32b12e46
zhangye <zy986@jqmkj.com>
2020-04-07 08:58:27 +0800

    111

---
+++ Library/Database/Index.php
@@ -0,0 +1,23 @@
+<?php
+namespace Library\Database;
+
+class Index 
+{
+
+    private static $instance;
+
+    static function setDb(){
+
+        if( ! self::$instance)
+
+    }
+
+    private function  __clone(){
+
+    }
+
+    static function test()
+    {
+        echo __NAMESPACE__;
+    }
+}
